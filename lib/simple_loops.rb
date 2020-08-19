41c124e57a2910a6300c1d840420a569374704dc
Casey B <caseyoksana@gmail.com>
2020-08-19 02:43:03 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -5,11 +5,13 @@ count = 0
 while count < 5 do
   puts message
   count += 1 
-end
+  end
 end
 
 def loop_message_n_times (message, number)
   count = 0 
   while count < number do 
     puts message
-    
+    count += 1 
+  end 
+end 

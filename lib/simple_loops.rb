4f4c74ae885e7745f8b310b7b8e6458a0d47337e
Casey B <caseyoksana@gmail.com>
2020-08-19 02:45:16 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -16,6 +16,12 @@ def loop_message_n_times (message, number)
   end 
 end 
 
-def output_array (array)
-  
-  
+def output_array(array)
+  count = 0
+  while count < array.length do
+    puts array[count]
+    count += 1
+  end
+end
+
+def string
